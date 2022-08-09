33d694c58b8357ce12f0fdd46f1bd5bf901b60c3
BlackyHat <105644022+BlackyHat@users.noreply.github.com>
2022-08-09 11:25:33 +0000

    Deploying to gh-pages from @ BlackyHat/demo-team@d8ad79564983b2e17a42485964f496891397c001 🚀

---
+++ index.2645f5c3.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,adaptiveHeight:!0,speed:500,easing:"linear"})}));
+//# sourceMappingURL=index.2645f5c3.js.map
---
+++ index.ccceca30.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,adaptiveHeight:!0,speed:500,easing:"linear"})}));
+//# sourceMappingURL=index.ccceca30.js.map
